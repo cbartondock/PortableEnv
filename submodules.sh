5a5d3c1ba908aae0f2f4863cc15165e31801040c
cbartondock <cbartondock@gmail.com>
2019-06-20 16:52:40 -0700

    fixed up zshrc

--- submodules.sh
+++ submodules.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-cd bundle
-for d in */ ; do
-  echo "downloading $d"
-  cd $d
-  git checkout master
-  git submodule update --init
-  git pull
-  cd ..
-done
-cd ..
+# cd bundle
+# for d in */ ; do
+#   echo "downloading $d"
+#   cd $d
+#   git checkout master
+#   git submodule update --init
+#   git pull
+#   cd ..
+# done
+# cd ..
+
+git pull --recurse-submodules
+git submodule-update --remote --recursive
