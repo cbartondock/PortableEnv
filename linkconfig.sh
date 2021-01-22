6e3af89c3fe400f104a82496c0902f1ba67935ec
cbartondock <cbartondock@gmail.com>
2021-01-22 14:06:33 -0500

    woof

--- linkconfig.sh
+++ linkconfig.sh
@@ -2,6 +2,16 @@
 for cfile in config_files/*; do
   [ -e "$cfile" ] || continue #don't go to glob if no file found
   name="$(basename "$cfile")"
-  echo "Linking $name to $HOME/.$name"
-  ln -s "$(cd "$HOME/.vim/config_files";pwd)/$name" "$HOME/.$name"
+  #if [ "$OSTYPE" = "msys" ]
+  #then
+    #echo "Linking $name to \"$(cygpath -w $HOME/.$name | sed -r 's/[\\]+/\\\\/g')\""
+    #echo "mklink /h \"$(cygpath -w $HOME/.$name | sed -r 's/[\\]+/\\\\/g')\" \"$(cygpath -w $HOME/.vim/config_files/$name | sed -r 's/[\\]+/\\\\/g')\" "
+
+    #cmd //c "mklink /h \"$(cygpath -w $HOME/.$name | sed -r 's/[\\]+/\\\\/g')\"  \"$(cygpath -w $HOME/.vim/config_files/$name | sed -r 's/[\\]+/\\\\/g')\""
+  #else
+
+    echo "Linking $name to $HOME/.$name"
+    rm $HOME/.$name
+    ln -s "$(cd "$HOME/.vim/config_files";pwd)/$name" "$HOME/.$name"
+  #fi
 done;
